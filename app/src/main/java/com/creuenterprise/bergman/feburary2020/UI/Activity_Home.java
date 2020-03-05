436e61b3eb59a2b6f4119f106167d429155acc88
cjmcintyre <coreymac193@hotmail.com>
2020-03-05 20:03:58 +1100

    Less comments pls
    Get better.

--- app/src/main/java/com/creuenterprise/bergman/feburary2020/UI/Activity_Home.java
+++ app/src/main/java/com/creuenterprise/bergman/feburary2020/UI/Activity_Home.java
@@ -1,8 +1,6 @@
 package com.creuenterprise.bergman.feburary2020.UI;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,7 +10,6 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.creuenterprise.bergman.feburary2020.R;
 import com.creuenterprise.bergman.feburary2020.UI.DB.LoginActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -23,7 +20,6 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class Activity_Home extends AppCompatActivity implements View.OnClickListener{
-
     EditText editTextName;
     TextView textEmail,textPhoneNumber,textEmailVerified;
     ProgressBar progressBar;
@@ -33,49 +29,36 @@ public class Activity_Home extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__home);
-
         user = FirebaseAuth.getInstance().getCurrentUser();
         editTextName = findViewById(R.id.edit_text_name);
         textEmail = findViewById(R.id.text_email);
-        //textPhoneNumber = findViewById(R.id.text_phone);
         textEmailVerified = findViewById(R.id.text_not_verified);
         progressBar = findViewById(R.id.progressbar);
-
         findViewById(R.id.text_not_verified).setOnClickListener(this);
         findViewById(R.id.button_save).setOnClickListener(this);
 
-
-        if(user!=null)
-        {
+        if (user != null) {
             editTextName.setText(user.getDisplayName());
             textEmail.setText(user.getEmail());
-            //textPhoneNumber.setText((user.getPhoneNumber().isEmpty()) ? "Add Number" : user.getPhoneNumber());
-
+            
             if(user.isEmailVerified())
                 textEmailVerified.setVisibility(View.INVISIBLE);
             else
                 textEmailVerified.setVisibility(View.VISIBLE);
         }
-
-        /*******************************
-         * Bottom Nav Bar
-         */
-        //Initialize List and set value
+        
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        //Set Home Selected
         bottomNavigationView.setSelectedItemId(R.id.home);
-        //Perform ItemSelectedListener
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch(menuItem.getItemId())
-                {
+                switch(menuItem.getItemId()) {
                     case R.id.dashboard:
                         startActivity(new Intent(getApplicationContext()
                                 , DashboardActivity.class));
                         overridePendingTransition(0,0);
                         return true;
-
+                        
                     case R.id.home:
                         return true;
 
@@ -89,12 +72,10 @@ public class Activity_Home extends AppCompatActivity implements View.OnClickList
             }
         });
     }
-
+    
     @Override
-    public void onClick(View v)
-    {
-        switch(v.getId())
-        {
+    public void onClick(View v) {
+        switch(v.getId()) {
             case (R.id.button_save):
                 save();
                 break;
@@ -104,20 +85,16 @@ public class Activity_Home extends AppCompatActivity implements View.OnClickList
         }
     }
 
-    private void sendEmailConfirmation()
-    {
+    private void sendEmailConfirmation() {
         progressBar.setVisibility(View.VISIBLE);
         user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful())
-                {
+                if (task.isSuccessful()) {
                     Toast.makeText(Activity_Home.this,
                             "Verification Email Sent", Toast.LENGTH_LONG).show();
                     progressBar.setVisibility(View.INVISIBLE);
-                }
-                else
-                {
+                } else {
                     Toast.makeText(Activity_Home.this,
                             "Something Went Wrong! " + task.getException(), Toast.LENGTH_LONG).show();
                     progressBar.setVisibility(View.INVISIBLE);
@@ -125,9 +102,8 @@ public class Activity_Home extends AppCompatActivity implements View.OnClickList
             }
         });
     }
-
-    private void save()
-    {
+    
+    private void save() {
         try {
             String name = editTextName.getText().toString().trim();
             if (name.isEmpty()) {
@@ -135,8 +111,7 @@ public class Activity_Home extends AppCompatActivity implements View.OnClickList
                 editTextName.requestFocus();
                 return;
             }
-
-
+            
             UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
                     .setDisplayName(name).build();
 
@@ -158,32 +133,23 @@ public class Activity_Home extends AppCompatActivity implements View.OnClickList
         }catch(Exception e) {}
     }
 
-    /*********************************
-     * Creates top menu / checks touch
-     */
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_my_profile, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch(item.getItemId())
-        {
-            //LOGOUT
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
             case R.id.action_add_contact:
                 FirebaseAuth.getInstance().signOut();
-
                 Intent i = new Intent(this, LoginActivity.class);
                 i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(i);
                 overridePendingTransition(0,0);
                 return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
